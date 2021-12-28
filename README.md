102468e6180121e158eeb99221e5611f32ff7bb2
marthamwangi <martyturing@gmail.com>
2021-12-28 19:56:44 +0300

    node service oriented architecture

---
+++ README.md
@@ -0,0 +1,15 @@
+# Redefinig Real Estate
+
+- styling : editorconfig
+- package manager : npm
+- dev & prod webserver : express
+- browsersync : cross device testing
+- WIP hosting :
+- Module : ES
+- Compiler & Transpiler : Babel
+- Bundler : Webpack
+- Linting : es-lint
+- Unit Testing : Mocha
+- Assertion Lib : Chai
+- Helper Lib : JSDOM
+- CI : Jenkins
---
+++ src/index.test.js
@@ -0,0 +1,30 @@
+import {expect} from "chai";
+import jsdom from "jsdom";
+import fs from "fs";
+
+describe ("First test", () =>
+{
+   it("should pass", () =>
+    {
+        expect(true).to.equal(true);
+    });
+});
+
+describe ("index.html", () =>{
+    it("should say Hello World!", () =>
+    {
+        const index = fs.readFileSync("./src/index.html","utf-8");
+        const {JSDOM} = jsdom;
+        const dom = new JSDOM(index);
+        const h2 = dom.window.document.getElementsByTagName("h2")[0];
+        expect(h2.innerHTML).to.equal("Hello World!");
+        dom.window.close();
+    });
+});
+describe ("First test", () =>
+{
+   it("should pass", () =>
+    {
+        expect(true).to.equal(true);
+    });
+});
